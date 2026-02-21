8d2584276308cb271d5950ef42c102562406fc38
NMRSantos <170880035+NMRSantos@users.noreply.github.com>
2026-02-21 00:24:15 +0000

    Created a addBookToLibrary() to create and add books to myLibrary[]

--- scripts.js
+++ scripts.js
@@ -1,9 +1,23 @@
 const myLibrary = [];
 
+const name = "The Big Book";
+const author = "Hungus Chungus";
+const pages = "67";
+const status = "It Exists";
+const id = crypto.randomUUID();
+
 function Book(name, author, pages, status, id) {
     this.name = name;
     this.author = author;
     this.pages = pages;
     this.status = status;
     this.id = id;
+};
+
+function addBookToLibrary() {
+    const book = new Book(name, author, pages, status, id);
+    console.log(book)
+
+    myLibrary.push = book
+    console.log(myLibrary)
 };
