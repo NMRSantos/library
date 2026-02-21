e3df571aa1bcec74210b91140355e45f14e58ede
NMRSantos <170880035+NMRSantos@users.noreply.github.com>
2026-02-21 00:01:15 +0000

    Created initial files and Book constructor

---
+++ scripts.js
@@ -0,0 +1,9 @@
+const myLibrary = [];
+
+function Book(name, author, pages, status, id) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+    this.id = id;
+};
